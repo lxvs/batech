ed5dd3862ffd67006b7c3d62f6b39dc79b934326
lxvs <jn.apsd@gmail.com>
2021-07-11 16:46:38 +0800

    Update assert

--- assert.bat
+++ assert.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal
 
-set vara=a
+set vara=10
 set varb=
 
 call:Assert "defined vara" "ERROR: vara is not defined!" || exit /b 1
 call:Assert "not defined varb" "ERROR: varb has been defined!" || exit /b 2
-call:Assert "defined varb" "ERROR: varb is not defined!" || exit /b 3
+@REM call:Assert "defined varb" "ERROR: varb is not defined!" || exit /b 3
+call:Assert "%%vara%% GTR 0" "ERROR: vara should be greater than 0!" || exit /b 4
+call:Assert "%%vara%% LSS 0" "ERROR: vara should be smaller than 0!" || exit /b 5
 
 pause
 exit /b
