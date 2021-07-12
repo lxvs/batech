7502f932057ddf532f9a98735e5407be75cf1314
lxvs <jn.apsd@gmail.com>
2021-07-12 23:28:13 +0800

    Update assert - %% to %

--- assert.bat
+++ assert.bat
@@ -7,8 +7,8 @@ set varb=
 call:Assert "defined vara" "ERROR: vara is not defined!" || exit /b 1
 call:Assert "not defined varb" "ERROR: varb has been defined!" || exit /b 2
 @REM call:Assert "defined varb" "ERROR: varb is not defined!" || exit /b 3
-call:Assert "%%vara%% GTR 0" "ERROR: vara should be greater than 0!" || exit /b 4
-call:Assert "%%vara%% LSS 0" "ERROR: vara should be smaller than 0!" || exit /b 5
+call:Assert "%vara% GTR 0" "ERROR: vara should be greater than 0!" || exit /b 4
+call:Assert "%vara% LSS 0" "ERROR: vara should be smaller than 0!" || exit /b 5
 
 pause
 exit /b
